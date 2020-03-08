f52fc1aa85f8a4b9939995293cc540251c22f7c0
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-03-08 12:23:55 +0100

    base working

--- apiary/urls.py
+++ apiary/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from beehive.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name="index-view")
 ]
