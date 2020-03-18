afa449857ed1c38b33930d96adb448837117d6a1
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-03-18 09:43:53 +0100

    heroku setttings

--- apiary/settings.py
+++ apiary/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'beehive/static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'beehive/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 django_heroku.settings(locals())
 
