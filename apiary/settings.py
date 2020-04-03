c79962cf10187b16e223f4d7e2476d063223fe31
Bartek Stokłosa <b_stoklosa@o2.pl>
2020-04-03 13:32:27 +0200

    heroku

--- apiary/settings.py
+++ apiary/settings.py
@@ -140,6 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'beehive/static')]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
